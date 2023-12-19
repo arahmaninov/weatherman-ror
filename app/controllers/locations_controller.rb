505295a23ec09d033621f5e1817d3a09b059311c
arahmaninov <arahmaninov@yandex.ru>
2023-12-19 18:49:33 +0300

    add locations controller

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,30 @@
+class LocationsController < ApplicationController
+  def index
+    # all locations
+  end
+
+  def show
+    # specific location
+  end
+
+  def new
+    # get a form to create a new location
+  end
+
+  def create
+    # create a new location from the data in 'new'
+  end
+
+  def edit
+    # get a form to update a location
+  end
+
+  def update
+    # update a location from the data in 'edit'
+  end
+
+  def destroy
+    # delete a location
+  end
+
+end
